32a25bd7725dfa4a18205f01cc4162d357195fcd
yudhaginongpratidina <yudhaginong071102@gmail.com>
2023-09-21 08:08:56 +0700

    Menambahkan Delete Contact

--- src/Components/ContactItem.jsx
+++ src/Components/ContactItem.jsx
@@ -1,14 +1,17 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ContactItemBody from './ContactItemBody';
 import ContactItemImage from './ContactItemImage';
+import DeleteButton from './DeleteButton';
  
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({  imageUrl, name, tag, id, onDelete }) {
  return (
    <div className="contact-item">
      <ContactItemImage imageUrl={imageUrl} />
      <ContactItemBody name={name} tag={tag} />
+     <DeleteButton id={id} onDelete={onDelete}/>
    </div>
  );
 }
---
+++ src/Components/DeleteButton.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function DeleteButton ( {id, onDelete} ){
+    return <button className="contact-item__delete" onClick={() => onDelete(id)}></button>
+}
+
+export default DeleteButton;
